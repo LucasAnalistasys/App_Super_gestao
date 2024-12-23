36eb412707d74d407bdbef4e363f3a5f35abcce8
LucasAnalistasys <lucascdarkron@gmail.com>
2024-12-23 11:16:11 -0300

    Criação Controllers 'PrincipalController' 'ContatoController' 'SobreNosController'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
+/* Rotas definidas com callback
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Olá, seja Bem vindo!";
 });
+
+
+Route::get('/sobrenos', function () {
+    return "Sobre nós";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
+});
+
+
+*/
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
+Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class,'sobrenos']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
+
+
+
+
+/* Verbo http
+
+get
+post
+put
+patch
+delete
+options 
+
+*/
