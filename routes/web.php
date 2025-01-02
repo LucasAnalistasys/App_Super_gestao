fbe91b670f1734670e02e752bd4e671862b32c24
LucasAnalistasys <lucascdarkron@gmail.com>
2025-01-02 09:52:53 -0300

    Redirecionamento de Rotas

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,16 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', function(){ return 'Produtos'; });
 });
 
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+/* Route::redirect('/rota2','/rota1'); */
+
 
 
 //nome, categoria, assunto, mensagem
