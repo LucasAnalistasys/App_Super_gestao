6983115c56cebd5ea4c868f507fdd4b3f05b7da8
LucasAnalistasys <lucascdarkron@gmail.com>
2025-01-04 14:41:37 -0300

    Uso do If Ternário

--- app/Http/Controllers/ListaFornecedores.php
+++ app/Http/Controllers/ListaFornecedores.php
@@ -11,6 +11,11 @@ class ListaFornecedores extends Controller
             0 => ['id' => '1' ,'nome'=> 'Fornecedor 1', 'status' => 'N', 'cnpj' => '000.000.000.00/0000' ],
             1 => ['id' => '2' ,'nome' => 'Fornecedor 2', 'status' => 'S', 'cnpj' => '']
         ];
+
+        // If Ternário - condiçao se ? se verdade : se falso
+        $msg =  @isset($fornecedores[1]['cnpj']) ? 'CNPJ Informado' : 'CNPJ Não Informado';
+        echo $msg;
+
         return view('app.fornecedores.lista', compact('fornecedores'));
     }
 }
