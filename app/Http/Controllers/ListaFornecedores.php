71a18bd8fe1414634c321098cfc129082a333e5e
LucasAnalistasys <lucascdarkron@gmail.com>
2025-01-06 09:10:57 -0300

    Uso de @cases

--- app/Http/Controllers/ListaFornecedores.php
+++ app/Http/Controllers/ListaFornecedores.php
@@ -8,9 +8,25 @@ class ListaFornecedores extends Controller
 {
     public function index(){
         $fornecedores = [
-            0 => ['id' => '1' ,'nome'=> 'Fornecedor 1', 'status' => 'N', 'cnpj' => '000.000.000.00/0000' ],
-            1 => ['id' => '2' ,'nome' => 'Fornecedor 2', 'status' => 'S', 'cnpj' => ''],
-            2 => ['id' => '3' ,'nome' => 'Fornecedor 3', 'status' => 'S']
+            0 => ['id' => '1' ,
+                'nome'=> 'Fornecedor 1', 
+                'status' => 'N', 
+                'cnpj' => '000.000.000.00/0000', 
+                'ddd' => '31',
+                'telefone' => '0000-0000' ],
+
+            1 => ['id' => '2' ,
+                  'nome' => 'Fornecedor 2', 
+                  'status' => 'S', 
+                  'cnpj' => '',
+                  'ddd' => '11',
+                  'telefone' => '1111-1110'],
+
+            2 => ['id' => '3' ,
+                  'nome' => 'Fornecedor 3', 
+                  'status' => 'S',
+                  'ddd' => '11',
+                  'telefone' => '2222-2222']
         ];
 
         // If Ternário - condiçao se ? se verdade : se falso
--- resources/views/app/fornecedores/lista.blade.php
+++ resources/views/app/fornecedores/lista.blade.php
@@ -45,11 +45,11 @@
         @endempty    
 
     @endisset
-@endisset    
+  
 
     <br/><br/>
     ________________________________________________
-    
+
     <br/><br/>
 
     Fornecedor: {{$fornecedores [2]['nome']}}
@@ -60,7 +60,38 @@
     <br/>
 
     CNPJ: {{ $fornecedores [2]['cnpj'] ?? 'Não Preenchido'}}
+    <!-- verifica se Variável testada está definida (isset)
+         ou
+         Verifica se Variável testada retorna valor null
+    -->
+    <br/>
+    TELEFONE: ({{$fornecedores[2]['ddd']}}) {{$fornecedores[2]['telefone']}}
+<br/>
+    ESTADO:
+    <!-- Estrutura switch para identificar o estado com base no DDD do terceiro fornecedor -->
+    @switch($fornecedores[2]['ddd'])
 
+        <!-- Caso o DDD seja '31', identifica como Belo Horizonte - MG -->
+        @case('31')
+            BELO HORIZONTE - MG
+            @break
 
+        <!-- Caso o DDD seja '11', identifica como São Paulo - SP -->
+        @case('11')
+            SÃO PAULO - SP
+            @break
+
+        <!-- Caso o DDD seja '32', identifica como Juiz de Fora - MG -->
+        @case('32')
+            JUIZ DE FORA - MG
+            @break
+
+        <!-- Se o DDD não for identificado, exibe "Estado não Identificado" -->
+        @default
+            Estado não Identificado
+
+    @endswitch
+    <!-- Finaliza a estrutura switch -->
+@endisset     
     
 
