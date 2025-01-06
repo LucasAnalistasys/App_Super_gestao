702957d1e3f08991c54296aff0c4f23179b80737
LucasAnalistasys <lucascdarkron@gmail.com>
2025-01-06 09:38:10 -0300

    Uso do @while

--- resources/views/app/fornecedores/lista.blade.php
+++ resources/views/app/fornecedores/lista.blade.php
@@ -21,6 +21,7 @@
 @endempty
  --}}
 
+<h4> USO DO FOR </h4>
 
 @isset($fornecedores)
 
@@ -72,10 +73,72 @@
         @endswitch
         <!-- Finaliza a estrutura switch -->
         <hr>
-        
+
     @endfor    
 
-@endisset     
+@endisset    
+
+<h4> USO DO WHILE </h4>
+
+
+@isset($fornecedores)
+
+    <!-- No while, não temos o incremento como no for. While funciona assim: while(true)
+    Portanto, a variável deve ser definida dentro de um bloco php, e o incremento colocado 
+    dentro do laço tambem como um bloco php -->
+
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i])) 
+
+        Fornecedor: {{$fornecedores [$i]['nome']}}
+        <br/>
+
+        Status: {{$fornecedores [$i]['status']}}
+        <br/>
+
+        <!-- verifica se Variável testada está definida (isset)
+            ou
+            Verifica se Variável testada retorna valor null
+        -->
+        CNPJ: {{ $fornecedores [$i]['cnpj'] ?? 'Não Preenchido'}}
+        
+        <br/>
+        TELEFONE: ({{$fornecedores[$i]['ddd']}}) {{$fornecedores[$i]['telefone']}}
+        <br/>
+
+        {{-- O bloco switch é usado para determinar o estado do fornecedor com base no DDD. 
+        Cada caso representa um DDD específico e exibe o estado correspondente. Se nenhum DDD corresponder, o default será exibido. --}}
+        ESTADO:
+
+        @switch($fornecedores[$i]['ddd'])
+
+        
+            @case('31')
+                BELO HORIZONTE - MG
+                @break
+
+        
+            @case('11')
+                SÃO PAULO - SP
+                @break
+
+            @case('32')
+                JUIZ DE FORA - MG
+                @break
+
+    
+            @default
+                Estado não Identificado
+
+        @endswitch
+        <!-- Finaliza a estrutura switch -->
+
+        @php $i++ @endphp
+        <hr>
+        
+    @endwhile    
+
+@endisset    
 
 
     
