9d270358dd4d71e886d7179a711d42f98da2d4ff
LucasAnalistasys <lucascdarkron@gmail.com>
2025-01-08 10:16:02 -0300

    Envio Form method GET

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
    public function contato(){
+
+        var_dump($_GET); //Uso da variável global $_GET para receber um array contendo os dados recebidos pelo Method Get do form
         return view('site.contato');
    }
 }
